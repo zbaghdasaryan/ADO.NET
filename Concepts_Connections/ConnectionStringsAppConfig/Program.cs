73bcabce08b1653df9458278223755d7823dd584
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-20 23:45:28 +0400

    ConnectionStringConfig

---
+++ Concepts_Connections/ConnectionStringsAppConfig/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+
+// Использование файлов конфигурации позволяет изолировать информацию о подключении 
+// от остальной части приложения 
+
+namespace CBS.ADO_NET.ConnectionStringConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Объект ConnectionStringSettings представляет собой отдельную строку подключения в разделе строк подключения 
+            // конфигурационного файла
+            var setting = new ConnectionStringSettings
+            {
+                Name = "MyConnectionString1",     //имя строки подключения в конфигурационном файле
+                ConnectionString = @"Data Source=.\SQLEXPRESS;
+                                     Initial Catalog=ShopDB;
+                                     Integrated Security=True;"
+            };
+
+            Configuration config;  // Объект Config представляет конфигурационный файл
+            config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);  // Объект ConfigurationManager предоставляет доступ к файлам конфигурации
+            config.ConnectionStrings.ConnectionStrings.Add(setting);
+            config.Save();
+
+            Console.WriteLine("Строка подключения записана в конфигурационный файл.");
+
+            // Получение строки подключения.
+            Console.WriteLine(ConfigurationManager.ConnectionStrings["MyConnectionString1"].ConnectionString);
+        }
+    }
+}
---
+++ Concepts_Connections/Pooling/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SqlClient;
+
+// Технология connection pooling позволяет уменьшить затраты на открытие и закрытие соединения
+
+namespace CBS.ADO_NET.Pooling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string conStr = @"Data Source=DESKTOP-DTQ5Q8H;Initial Catalog=ShopDB;Integrated Security=True;Connect Timeout=30;
+            //    Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Pooling = true"; // включение или отключение пула для этого подключения
+
+            string conStr = @"Data Source=DESKTOP-DTQ5Q8H;Initial Catalog=ShopDB;Integrated Security=True;Connect Timeout=30;
+                                Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False; Pooling = false"; // включение или отключение пула для этого подключения
+
+            DateTime start = DateTime.Now;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                SqlConnection connection = new SqlConnection(conStr);
+                connection.Open();    // при включенном пуле физическое соединение не создается, а берется из пула соединений
+                connection.Close();   // при включенном пуле физическое соединение не разрывается, а помещается в пул
+            }
+
+            TimeSpan stop = DateTime.Now - start;
+
+            Console.WriteLine(stop.TotalSeconds);
+        }
+    }
+}
