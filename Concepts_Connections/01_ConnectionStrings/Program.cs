d0f570d1f0f758587f89f2828fc694a011a3b375
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2019-02-20 16:36:03 +0400

    ConnectionStrings1

---
+++ Concepts_Connections/01_ConnectionStrings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.SqlClient; // в этом пространстве имен находится поставщик данных для MS SQLServer
+
+namespace CBS.ADO_NET.ConnectionStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = @"Data Source=DESKTOP-DTQ5Q8H;Initial Catalog=ShopDB;Integrated Security=True;Connect Timeout=30;
+                                Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"; //строка подключения
+
+            // Следую инструкциям строки подключения следует найти на локальном компьютере экземпляр SQL Server  с именем SQLEXPRESS, 
+            // поискать каталог ShopDB и попытаться получить доступ к источнику данных через доверительное подключение, 
+            // используя для этого вашу учетную запись Microsoft Windows
+
+            SqlConnection connection = new SqlConnection(conStr);
+
+            try
+            {
+                connection.Open(); // открытие физического подключения к источнику данных 
+                Console.WriteLine(connection.State);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                connection.Close(); //закрытие физического соединения с источником данных
+                Console.WriteLine(connection.State);
+            }
+        }
+    }
+}
---
+++ Concepts_Connections/Concepts_Connections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concepts_Connections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
